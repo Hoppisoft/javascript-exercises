ecef89214094c10c3c4fc4edb44239f734d9823c
Hoppisoft <81927738+Hoppisoft@users.noreply.github.com>
2024-10-02 16:20:36 +0200

    03_reverseString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+  let returnWord = ''; 
 
+  if (times < 0) {
+    return 'ERROR';
+  };
+  
+  for (let index = 0; index < times; index++) {
+    returnWord += word;
+    
+  }
+  return returnWord;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(origString) {
+  let returnString = '';
+  index = origString.length;
 
+  for (let i = 0; i < origString.length; i++) {
+    index--;
+    returnString += origString[index];
+   
+  }
+
+  return returnString;
 };
 
 // Do not edit below this line
